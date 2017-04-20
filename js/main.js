71f0f637e2a10d6bbcdf556e1dd3205dbe066a66
wangsongiam <wangsongiam@gmail.com>
2017-04-19 20:27:16 -0400

    add another sushi

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 
-	var animations=['shake', 'grow'];
+	var animations=['shake', 'shake2'];
 
 	function getRandomNumber(min, max){
 
 		return Math.floor(Math.random()*(max-min+1)+ min );
 	}//end of function
 
-	$("#sushi1").on("click", function(){ 
+	$("#sushi2").on("click", function(){ 
 		var sushi=this;
 		var animation=animations[getRandomNumber(0,1)];
 
